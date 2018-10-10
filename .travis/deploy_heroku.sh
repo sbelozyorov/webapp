416f67c0634e219ed8cfba07f0d5503096a8c78c
sbelozyorov <sbelozyorov@gmail.com>
2018-10-10 22:42:46 +0300

    Dancing with Heroku

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+#wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 docker login --username $HEROKU_LOGIN --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
+docker build -t registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
