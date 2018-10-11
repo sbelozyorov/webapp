fc1f6e96c3b0df3d7d8e70b8fca88c3b7ebd79fe
sbelozyorov <sbelozyorov@gmail.com>
2018-10-11 18:14:50 +0300

    Commit

---
+++ .travis/ecs_deploy.sh
@@ -0,0 +1,17 @@
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+add-apt-repository ppa:eugenesan/ppa
+apt-get update
+apt-get install jq -y
+
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | sudo tee -a /usr/bin/ecs-deploy
+sudo chmod +x /usr/bin/ecs-deploy
+
+# Use this for AWS ECR
+eval $(aws ecr get-login --no-include-email --region us-west-1)
+docker build -t sbelozyorov@webapp
+docker tag sbelozyorov/webapp:latest 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
+docker push 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
+
+ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
