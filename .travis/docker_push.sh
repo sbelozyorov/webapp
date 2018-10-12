8cc7c6283b9f0dd8446c7eb72c7b73ca84e2cbd3
sbelozyorov <sbelozyorov@gmail.com>
2018-10-12 16:52:38 +0300

    Update deploy script

--- .travis/docker_push.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-pip install --user awscli
-export PATH=$PATH:$HOME/.local/bin
-eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
-docker build -t sbelozyorov@webapp .
-docker tag sbelozyorov/webapp:latest 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
-docker push 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
--- scripts/ecs_deploy.sh
+++ scripts/ecs_deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-add-apt-repository ppa:jonathonf/python-3.6
-sudo apt-get update
-sudo apt-get update
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 
+# Install jq
 add-apt-repository ppa:eugenesan/ppa
 apt-get update
 apt-get install jq -y
 
+# Download ecs-deploy script
 curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | sudo tee /usr/bin/ecs-deploy
 sudo chmod +x /usr/bin/ecs-deploy
 
-# Use this for AWS ECR
+# Push image to AWS ECR
 eval $(aws ecr get-login --no-include-email --region us-west-1)
 docker build -t sbelozyorov@webapp
 docker tag sbelozyorov/webapp:latest 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
 docker push 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
 
+# Deploy application
 ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
