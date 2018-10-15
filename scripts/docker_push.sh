b8c448e27fa22be8241a38f918dc44973452e943
sbelozyorov <sbelozyorov@gmail.com>
2018-10-15 19:15:51 +0300

    Updating scripts

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH
-  ; fi`
-export $DOCKER_USER/webapp
-docker build -t $DOCKER_USER/webapp .
-docker tag $DOCKER_USER/webapp $DOCKER_USER/javawebapp
-docker push $DOCKER_USER/webapp
+#docker login -u $DOCKER_USER -p $DOCKER_PASS
+#export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH
+#  ; fi`
+#export $DOCKER_USER/webapp
+#docker build -t $DOCKER_USER/webapp .
+#docker tag $DOCKER_USER/webapp $DOCKER_USER/javawebapp
+#docker push $DOCKER_USER/webapp
