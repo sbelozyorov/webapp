a24100fef591ce6e44133133c98917f6fc0c4a66
sbelozyorov <sbelozyorov@gmail.com>
2018-10-15 19:29:33 +0300

    Updating scripts

--- scripts/ecs_deploy.sh
+++ scripts/ecs_deploy.sh
@@ -12,10 +12,18 @@ curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-de
 sudo chmod +x /usr/bin/ecs-deploy
 
 # Push image to AWS ECR
-eval $(aws ecr get-login --no-include-email --region us-west-1)
-docker build -t sbelozyorov@webapp
-docker tag sbelozyorov/webapp:latest 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
-docker push 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
+#eval $(aws ecr get-login --no-include-email --region us-west-1)
+#docker build -t sbelozyorov@webapp
+#docker tag sbelozyorov/webapp:latest 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
+#docker push 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
 
 # Deploy application
-ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
+#ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH
+  ; fi`
+export $DOCKER_USER/webapp
+docker build -t $DOCKER_USER/webapp .
+docker tag $DOCKER_USER/webapp $DOCKER_USER/javawebapp
+docker push $DOCKER_USER/webapp
+ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $DOCKER_USER/webapp
