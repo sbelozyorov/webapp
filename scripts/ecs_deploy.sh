669491c83c1de7e9c418783d3916149638886588
sbelozyorov <sbelozyorov@gmail.com>
2018-10-15 19:42:24 +0300

    Updating scripts

--- scripts/ecs_deploy.sh
+++ scripts/ecs_deploy.sh
@@ -13,17 +13,9 @@ sudo chmod +x /usr/bin/ecs-deploy
 
 # Push image to AWS ECR
 eval $(aws ecr get-login --no-include-email --region us-west-1)
-#docker build -t sbelozyorov@webapp
-#docker tag sbelozyorov/webapp:latest 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
-#docker push 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
+docker build -t sbelozyorov@webapp
+docker tag sbelozyorov/webapp:latest 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
+docker push 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
 
 # Deploy application
-#ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH
-  ; fi`
-export $DOCKER_USER/webapp
-docker build -t $DOCKER_USER/webapp .
-docker tag $DOCKER_USER/webapp $DOCKER_USER/javawebapp
-docker push $DOCKER_USER/webapp
-ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i sb
+ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i 434952379752.dkr.ecr.us-west-1.amazonaws.com/sbelozyorov/webapp:latest
